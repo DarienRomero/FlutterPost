478fb0eed5d48f62dfaf412f627e9a9697a14d21
Darien Romero <darien.romero@mediabyte.com.pe>
2025-02-02 18:20:34 -0500

    ui: folder_tree

--- lib/features/collections/presentation/widgets/folder_tree.dart
+++ lib/features/collections/presentation/widgets/folder_tree.dart
@@ -19,9 +19,81 @@ class FolderTree extends StatelessWidget {
           ),
         )
       ),
-      child: const Center(
-        child: Text('Main Content'),
-      ),
+      child: ApiList()
     );
   }
+}
+
+class ApiList extends StatelessWidget {
+  final List<ApiFolder> folders = [
+    ApiFolder("Oriapp", [
+      ApiEndpoint("POST", "/login"),
+      ApiEndpoint("POST", "/logout"),
+      ApiEndpoint("GET", "/negocios"),
+      ApiEndpoint("GET", "/locales"),
+      ApiEndpoint("GET", "/almacenes"),
+      ApiEndpoint("GET", "/categorias"),
+      ApiFolder("Catálogo", [
+        ApiEndpoint("POST", "/catalogo-todos"),
+        ApiEndpoint("POST", "/catalogo-codigo"),
+        ApiEndpoint("POST", "/catalogo-nombre"),
+        ApiEndpoint("POST", "/catalogo-filtro"),
+      ]),
+      ApiEndpoint("POST", "/vendedores"),
+      ApiEndpoint("POST", "/mostrar-igv"),
+      ApiEndpoint("POST", "/insertar-orden"),
+      ApiFolder("Órdenes", [
+        ApiEndpoint("GET", "/ordenes"),
+        ApiEndpoint("GET", "/detalle-orden"),
+      ]),
+      ApiEndpoint("POST", "/clientes"),
+    ]),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.zero,
+      children: folders.map((folder) => buildFolder(folder)).toList(),
+    );
+  }
+
+  Widget buildFolder(ApiFolder folder) {
+    return ExpansionTile(
+      iconColor: Colors.white,
+      collapsedIconColor: Colors.white.withOpacity(0.6),
+      title: Text(folder.name, style: const TextStyle(fontWeight: FontWeight.bold, color: Colors.white)),
+      children: folder.children.map((child) {
+        if (child is ApiFolder) {
+          return buildFolder(child);
+        } else if (child is ApiEndpoint) {
+          return ListTile(
+            title: Text(child.path, style: const TextStyle(
+              color: Colors.white
+            )),
+            leading: Text(
+              child.method,
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: child.method == "GET" ? Colors.green : Colors.orange,
+              ),
+            ),
+          );
+        }
+        return const SizedBox.shrink();
+      }).toList(),
+    );
+  }
+}
+
+class ApiFolder {
+  final String name;
+  final List<dynamic> children; // Puede contener `ApiFolder` o `ApiEndpoint`
+  ApiFolder(this.name, this.children);
+}
+
+class ApiEndpoint {
+  final String method;
+  final String path;
+  ApiEndpoint(this.method, this.path);
 }
